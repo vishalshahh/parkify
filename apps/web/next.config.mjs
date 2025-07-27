70ace416ebf88897b96117350930f7c6e8d70322
vishalshahh <vishal12kl@gmail.com>
2025-07-27 15:22:24 +0530

    fix: watching unwanted errors

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    esmExternals: 'loose',
   },
   transpilePackages: ['@parkify/ui', '@parkify/forms', '@parkify/network', '@parkify/util'],
   images: {
